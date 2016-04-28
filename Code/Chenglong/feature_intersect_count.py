fa6dc4f35e18f0fb5193c028956a0dd1e9de59fe
ChenglongChen <c.chenglong@gmail.com>
2016-04-29 03:00:55 +0800

    intersect features

---
+++ Code/Chenglong/feature_intersect_count.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Chenglong Chen <c.chenglong@gmail.com>
+@brief: intersect count features
+
+"""
+
+import re
+import string
+
+import numpy as np
+import pandas as pd
+
+import config
+from utils import dist_utils, ngram_utils, nlp_utils, np_utils
+from utils import logging_utils, time_utils, pkl_utils
+from feature_base import BaseEstimator, PairwiseFeatureWrapper
+
+
+# tune the token pattern to get a better correlation with y_train
+# token_pattern = r"(?u)\b\w\w+\b"
+# token_pattern = r"\w{1,}"
+# token_pattern = r"\w+"
+# token_pattern = r"[\w']+"
+token_pattern = " " # just split the text into tokens
+
+
+# ----------------------------------------------------------------------------
+# How many ngrams of obs are in target?
+# Obs: [AB, AB, AB, AC, DE, CD]
+# Target: [AB, AC, AB, AD, ED]
+# ->
+# IntersectCount: 4 (i.e., AB, AB, AB, AC)
+# IntersectRatio: 4/6
+class IntersectCount_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode="", 
+        str_match_threshold=config.STR_MATCH_THRESHOLD):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        self.str_match_threshold = str_match_threshold
+        
+    def _get_feat_name(self):
+        return "IntersectCount_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        s = 0.
+        for w1 in obs_ngrams:
+            for w2 in target_ngrams:
+                if dist_utils._is_str_match(w1, w2, self.str_match_threshold):
+                    s += 1.
+                    break
+        return s
+
+
+class IntersectRatio_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode="", 
+        str_match_threshold=config.STR_MATCH_THRESHOLD):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        self.str_match_threshold = str_match_threshold
+        
+    def _get_feat_name(self):
+        return "IntersectRatio_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        s = 0.
+        for w1 in obs_ngrams:
+            for w2 in target_ngrams:
+                if dist_utils._is_str_match(w1, w2, self.str_match_threshold):
+                    s += 1.
+                    break
+        return np_utils._try_divide(s, len(obs_ngrams))
+
+
+# ----------------------------------------------------------------------------
+# How many cooccurrence ngrams between obs and target?
+# Obs: [AB, AB, AB, AC, DE, CD]
+# Target: [AB, AC, AB, AD, ED]
+# ->
+# CooccurrenceCount: 7 (i.e., AB x 2 + AB x 2 + AB x 2 + AC x 1)
+# CooccurrenceRatio: 7/(6 x 5)
+class CooccurrenceCount_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode="", str_match_threshold=config.STR_MATCH_THRESHOLD):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        self.str_match_threshold = str_match_threshold
+        
+    def _get_feat_name(self):
+        return "CooccurrenceCount_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        s = 0.
+        for w1 in obs_ngrams:
+            for w2 in target_ngrams:
+                if dist_utils._is_str_match(w1, w2, self.str_match_threshold):
+                    s += 1.
+        return s
+
+
+class CooccurrenceRatio_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode="", str_match_threshold=config.STR_MATCH_THRESHOLD):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        self.str_match_threshold = str_match_threshold
+        
+    def _get_feat_name(self):
+        return "CooccurrenceRatio_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        s = 0.
+        for w1 in obs_ngrams:
+            for w2 in target_ngrams:
+                if dist_utils._is_str_match(w1, w2, self.str_match_threshold):
+                    s += 1.
+        return np_utils._try_divide(s, len(obs_ngrams)*len(target_ngrams))
+
+
+# ---------------------------- Main --------------------------------------
+def main():
+    logname = "generate_feature_intersect_count_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED_STEMMED)
+
+    generators = [
+        IntersectCount_Ngram, 
+        IntersectRatio_Ngram, 
+        CooccurrenceCount_Ngram, 
+        CooccurrenceRatio_Ngram,
+    ]
+    obs_fields_list = []
+    target_fields_list = []
+    ## query in document
+    obs_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"] )
+    target_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"] )
+    ## document in query
+    obs_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"] )
+    target_fields_list.append( ["search_term", "search_term_product_name", 'search_term_alt", "search_term_auto_corrected"] )
+    ngrams = [1,2,3,12,123][:3]
+    for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+        for generator in generators:
+            for ngram in ngrams:
+                param_list = [ngram]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Code/Chenglong/feature_intersect_position.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Chenglong Chen <c.chenglong@gmail.com>
+@brief: intersect position features
+
+"""
+
+import re
+import string
+
+import numpy as np
+import pandas as pd
+
+import config
+from utils import dist_utils, ngram_utils, nlp_utils, np_utils
+from utils import logging_utils, time_utils, pkl_utils
+from feature_base import BaseEstimator, PairwiseFeatureWrapper
+
+
+# tune the token pattern to get a better correlation with y_train
+# token_pattern = r"(?u)\b\w\w+\b"
+# token_pattern = r"\w{1,}"
+# token_pattern = r"\w+"
+# token_pattern = r"[\w']+"
+token_pattern = " " # just split the text into tokens
+
+
+def _inter_pos_list(obs, target):
+    """
+        Get the list of positions of obs in target
+    """
+    pos_list = [0]
+    if len(obs) != 0:
+        pos_list = [i for i,o in enumerate(obs, start=1) if o in target]
+        if len(pos_list) == 0:
+            pos_list = [0]
+    return pos_list
+
+
+def _inter_norm_pos_list(obs, target):
+    pos_list = _inter_pos_list(obs, target)
+    N = len(obs)
+    return [np_utils._try_divide(i, N) for i in pos_list]
+
+
+class IntersectPosition_Ngram(BaseEstimator):
+    """Single aggregation features"""
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        
+    def _get_feat_name(self):
+        if isinstance(self.aggregation_mode, str):
+            feat_name = "IntersectPosition_%s_%s"%(
+                self.ngram_str, string.capwords(self.aggregation_mode))
+        elif isinstance(self.aggregation_mode, list):
+            feat_name = ["IntersectPosition_%s_%s"%(
+                self.ngram_str, string.capwords(m)) for m in self.aggregation_mode]
+        return feat_name
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        pos_list = _inter_pos_list(obs_ngrams, target_ngrams)
+        return pos_list
+
+
+class IntersectNormPosition_Ngram(BaseEstimator):
+    """Single aggregation features"""
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+        
+    def _get_feat_name(self):
+        if isinstance(self.aggregation_mode, str):
+            feat_name = "IntersectNormPosition_%s_%s"%(
+                self.ngram_str, string.capwords(self.aggregation_mode))
+        elif isinstance(self.aggregation_mode, list):
+            feat_name = ["IntersectNormPosition_%s_%s"%(
+                self.ngram_str, string.capwords(m)) for m in self.aggregation_mode]
+        return feat_name
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        pos_list = _inter_norm_pos_list(obs_ngrams, target_ngrams)
+        return pos_list
+
+
+# ---------------------------- Main --------------------------------------
+def main():
+    logname = "generate_feature_intersect_position_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED_STEMMED)
+
+    generators = [
+        IntersectPosition_Ngram, 
+        IntersectNormPosition_Ngram, 
+    ]
+    obs_fields_list = []
+    target_fields_list = []
+    ## query in document
+    obs_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"] )
+    target_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"][1:2] )
+    ## document in query
+    obs_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"][1:2] )
+    target_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"] )
+    ngrams = [1,2,3,12,123][:3]
+    aggregation_mode = ["mean", "std", "max", "min", "median"]
+    for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+        for generator in generators:
+            for ngram in ngrams:
+                param_list = [ngram, aggregation_mode]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+
+if __name__ == "__main__":
+    main()
