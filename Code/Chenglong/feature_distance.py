8379bdd9f7f7d337f7dbe9333a55a91d54b904b2
ChenglongChen <c.chenglong@gmail.com>
2016-04-29 02:42:00 +0800

    distance & doc2vec features

---
+++ Code/Chenglong/feature_distance.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Chenglong Chen <c.chenglong@gmail.com>
+@brief: distance features
+
+"""
+
+import re
+import sys
+import string
+
+import numpy as np
+import pandas as pd
+
+import config
+from utils import dist_utils, ngram_utils, nlp_utils
+from utils import logging_utils, time_utils, pkl_utils
+from feature_base import BaseEstimator, PairwiseFeatureWrapper
+
+
+# tune the token pattern to get a better correlation with y_train
+# token_pattern = r"(?u)\b\w\w+\b"
+# token_pattern = r"\w{1,}"
+# token_pattern = r"\w+"
+# token_pattern = r"[\w']+"
+token_pattern = " " # just split the text into tokens
+
+
+# ------------------- Jaccard & Dice --------------------------------------
+class JaccardCoef_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+
+    def _get_feat_name(self):
+        return "JaccardCoef_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        return dist_utils._jaccard_coef(obs_ngrams, target_ngrams)
+
+
+class DiceDistance_Ngram(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+
+    def _get_feat_name(self):
+        return "DiceDistance_%s"%self.ngram_str
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        return dist_utils._dice_dist(obs_ngrams, target_ngrams)
+
+
+# ------------------ Edit Distance --------------------------------
+class EditDistance(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+    
+    def _get_feat_name(self):
+        return "EditDistance"
+
+    def transform_one(self, obs, target, id):
+        return dist_utils._edit_dist(obs, target)
+
+
+class EditDistance_Ngram(BaseEstimator):
+	"""Double aggregation features"""
+    def __init__(self, obs_corpus, target_corpus, ngram, aggregation_mode_prev="", aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode, None, aggregation_mode_prev)
+        self.ngram = ngram
+        self.ngram_str = ngram_utils._ngram_str_map[self.ngram]
+
+    def _get_feat_name(self):
+        feat_name = []
+        for m1 in self.aggregation_mode_prev:
+            for m in self.aggregation_mode:
+                n = "EditDistance_%s_%s_%s"%(self.ngram_str, string.capwords(m1), string.capwords(m))
+                feat_name.append(n)
+        return feat_name
+
+    def transform_one(self, obs, target, id):
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        obs_ngrams = ngram_utils._ngrams(obs_tokens, self.ngram)
+        target_ngrams = ngram_utils._ngrams(target_tokens, self.ngram)
+        val_list = []
+        for w1 in obs_ngrams:
+            _val_list = []
+            for w2 in target_ngrams:
+                s = dist_utils._edit_dist(w1, w2)
+                _val_list.append(s)
+            if len(_val_list) == 0:
+                _val_list = [ config.MISSING_VALUE_NUMERIC ]
+            val_list.append( _val_list )
+        if len(val_list) == 0:
+            val_list = [ [config.MISSING_VALUE_NUMERIC] ]
+        return val_list
+
+
+# ------------------ Compression Distance --------------------------------
+class CompressionDistance(BaseEstimator):
+	"""Very time consuming"""
+    def __init__(self, obs_corpus, target_corpus, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+
+    def _get_feat_name(self):
+        return "CompressionDistance"
+
+    def transform_one(self, obs, target, id):
+        return dist_utils._compression_dist(obs, target)
+
+
+# ---------------------------- Main --------------------------------------
+def run_ngram_jaccard():
+    logname = "generate_feature_ngram_jaccard_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED_STEMMED)
+
+    generators = [JaccardCoef_Ngram, DiceDistance_Ngram]
+    obs_fields_list = []
+    target_fields_list = []
+    obs_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"][1:2] )
+    target_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"] )
+    ngrams = [1,2,3,12,123][:3]
+    for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+        for generator in generators:
+            for ngram in ngrams:
+                param_list = [ngram]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+
+def run_edit_distance():
+    logname = "generate_feature_edit_distance_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED_STEMMED)
+
+    obs_fields_list = []
+    target_fields_list = []
+    obs_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"][1:2] )
+    target_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"] )
+    ngrams = [1,2,3,12,123][:3]
+    aggregation_mode_prev = ["mean", "max", "min", "median"]
+    aggregation_mode = ["mean", "std", "max", "min", "median"]
+    for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+        param_list = []
+        PairwiseFeatureWrapper(EditDistance, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+        for ngram in ngrams:
+            param_list = [ngram, aggregation_mode_prev, aggregation_mode]
+            pf = PairwiseFeatureWrapper(EditDistance_Ngram, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+            pf.go()
+
+
+def run_compression_distance():
+    logname = "generate_feature_compression_distance_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED_STEMMED)
+
+    obs_fields_list = []
+    target_fields_list = []
+    obs_fields_list.append( ["search_term", "search_term_product_name", "search_term_alt", "search_term_auto_corrected"] )
+    target_fields_list.append( ["product_title", "product_title_product_name", "product_description", "product_attribute", "product_brand", "product_color"] )
+    for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+        param_list = []
+        pf = PairwiseFeatureWrapper(CompressionDistance, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+        pf.go()
+
+
+def main(which):
+    if which == "jaccard":
+        run_ngram_jaccard()
+    elif which == "edit":
+        run_edit_distance()
+    elif which == "compression":
+        run_compression_distance()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
---
+++ Code/Chenglong/feature_doc2vec.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Chenglong Chen <c.chenglong@gmail.com>
+@brief: doc2vec based features
+
+"""
+
+import gensim
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+import config
+from utils import dist_utils, ngram_utils, nlp_utils
+from utils import logging_utils, time_utils, pkl_utils
+from feature_base import BaseEstimator, StandaloneFeatureWrapper, PairwiseFeatureWrapper
+
+
+class Doc2Vec_BaseEstimator(BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, aggregation_mode)
+        self.model = doc2vec_model
+        self.sent_label = sent_label
+        self.model_prefix = model_prefix
+        self.vector_size = doc2vec_model.vector_size
+
+    def _get_vector(self, sent):
+        try:
+            vect = self.model.docvecs[self.sent_label[sent]]
+        except:
+            vect = np.zeros(self.vector_size, dtype=float)
+        return vect
+
+    def _get_cosine_sim(self, sent1, sent2):
+        vect1 = self._get_vector(sent1)
+        vect2 = self._get_vector(sent2)
+        return dist_utils._cosine_sim(vect1, vect2)
+
+    def _get_vdiff(self, sent1, sent2):
+        vect1 = self._get_vector(sent1)
+        vect2 = self._get_vector(sent2)
+        return dist_utils._diff(vect1, vect2)
+
+    def _get_rmse(self, sent1, sent2):
+        vect1 = self._get_vector(sent1)
+        vect2 = self._get_vector(sent2)
+        return dist_utils._rmse(vect1, vect2)
+
+
+class Doc2Vec_Vector(Doc2Vec_BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode)
+
+    def _get_feat_name(self):
+        return "Doc2Vec_%s_D%d_Vector"%(self.model_prefix, self.vector_size)
+
+    def transform_one(self, obs, target, id):
+        return self._get_vector(obs)
+
+
+class Doc2Vec_Vdiff(Doc2Vec_BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode)
+
+    def _get_feat_name(self):
+        return "Doc2Vec_%s_D%d_Vdiff"%(self.model_prefix, self.vector_size)
+
+    def transform_one(self, obs, target, id):
+        return self._get_vdiff(obs, target)
+
+
+class Doc2Vec_CosineSim(Doc2Vec_BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode)
+
+    def _get_feat_name(self):
+        return "Doc2Vec_%s_D%d_CosineSim"%(self.model_prefix, self.vector_size)
+
+    def transform_one(self, obs, target, id):
+        return self._get_cosine_sim(obs, target)
+
+
+class Doc2Vec_RMSE(Doc2Vec_BaseEstimator):
+    def __init__(self, obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode=""):
+        super().__init__(obs_corpus, target_corpus, doc2vec_model, sent_label, model_prefix, aggregation_mode)
+
+    def _get_feat_name(self):
+        return "Doc2Vec_%s_D%d_RMSE"%(self.model_prefix, self.vector_size)
+
+    def transform_one(self, obs, target, id):
+        return self._get_rmse(obs, target)
+
+
+# -------------------------------- Main ----------------------------------
+def main():
+    logname = "generate_feature_doc2vec_%s.log"%time_utils._timestamp()
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    #### NOTE: use data BEFORE STEMMING
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED)
+
+    doc2vec_model_dirs = []
+    model_prefixes = []
+    ## doc2vec model trained with Homedepot dataset: brand/color/obs/title/description
+    doc2vec_model_dirs.append( config.DOC2VEC_MODEL_DIR + "/Homedepot-doc2vec-D%d-min_count%d.model"%(config.EMBEDDING_DIM, config.EMBEDDING_MIN_COUNT) )
+    model_prefixes.append( "Homedepot" )
+    for doc2vec_model_dir, model_prefix in zip(doc2vec_model_dirs, model_prefixes):
+        ## load model
+        try:
+            if ".bin" in doc2vec_model_dir:
+                doc2vec_model = gensim.models.Doc2Vec.load_word2vec_format(doc2vec_model_dir, binary=True)
+            if ".txt" in doc2vec_model_dir:
+                doc2vec_model = gensim.models.Doc2Vec.load_word2vec_format(doc2vec_model_dir, binary=False)
+            else:
+                doc2vec_model = gensim.models.Doc2Vec.load(doc2vec_model_dir)
+                doc2vec_model_sent_label = pkl_utils._load(doc2vec_model_dir+".sent_label")
+        except:
+            continue
+
+        # ## standalone (not used in model building)
+        # obs_fields = ["search_term", "search_term_alt", "product_title", "product_description", "product_attribute"]
+        # generator = Doc2Vec_Vector
+        # param_list = [doc2vec_model, doc2vec_model_sent_label, model_prefix]
+        # sf = StandaloneFeatureWrapper(generator, dfAll, obs_fields, param_list, config.FEAT_DIR, logger)
+        # sf.go()
+
+        ## pairwise
+        generators = [
+            Doc2Vec_CosineSim, 
+            Doc2Vec_RMSE, 
+            Doc2Vec_Vdiff,
+        ]
+        obs_fields_list = []
+        target_fields_list = []
+        obs_fields_list.append( ["search_term", "search_term_alt"] )
+        target_fields_list.append( ["product_title", "product_description", "product_attribute", "product_brand", "product_color"] )
+        for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+            for generator in generators:
+                param_list = [doc2vec_model, doc2vec_model_sent_label, model_prefix]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+
+if __name__ == "__main__":
+    main()
